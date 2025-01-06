91001871a81e69976f93e6435fc09be24fb8cd82
Rama Sadea Putra <145627033+RamaSadeaPutra@users.noreply.github.com>
2025-01-06 14:42:30 +0700

    Update Query CREATE TABLE.sql
    Ubah kolom tabel

--- Rama Sadea Putra/Query CREATE TABLE.sql
+++ Rama Sadea Putra/Query CREATE TABLE.sql
@@ -1,46 +1,51 @@
-CREATE DATABASE pemesanan_tiket_pesawat
-USE pemesanan_tiket_pesawat;
 
 CREATE TABLE Penumpang(
     id_penumpang INT IDENTITY(1,1) PRIMARY KEY,
     nama_penumpang VARCHAR(50),
     no_hp_penumpang VARCHAR(25),
     no_ktp VARCHAR(25),
-    no_paspor VARCHAR(25)
+    no_paspor VARCHAR(25),
+	waktu_dibuat DATETIME DEFAULT GETDATE(),
+	waktu_diedit DATETIME DEFAULT GETDATE()
 );
+
 
 CREATE TABLE Bandara (
     id_bandara INT IDENTITY(1,1) PRIMARY KEY,
     nama_bandara VARCHAR(50),
-    kota VARCHAR(50),
-    negara VARCHAR(50)
+    nama_kota VARCHAR(50),
+    nama_negara VARCHAR(50)
 );
 
 CREATE TABLE Penerbangan (
     id_penerbangan INT IDENTITY(1,1) PRIMARY KEY,
     id_bandara INT,
     no_penerbangan VARCHAR(50),
+	nama_maskapai VARCHAR(50),
     tempat_tujuan VARCHAR(50),
 	bandara_tujuan VARCHAR(50),
-    harga DECIMAL(10,2),
-    estimasi_waktu DATETIME,
+    harga_tiket DECIMAL (10,2),
     waktu_keberangkatan DATETIME,
 	waktu_kedatangan DATETIME,
     FOREIGN KEY (id_bandara) REFERENCES Bandara(id_bandara)
 );
+
+
 
 CREATE TABLE Tiket (
     id_tiket INT IDENTITY(1,1) PRIMARY KEY,
     id_penerbangan INT,
     id_penumpang INT,
     nama_maskapai VARCHAR(50),
-	tanggal_pembayran DATETIME,
+	harga_tiket DECIMAL (10,2),
 	waktu_keberangkatan DATETIME,
 	tempat_tujuan VARCHAR(50),
 	bandara_tujuan VARCHAR(50),
 	nama_penumpang VARCHAR(50),
 	no_kursi VARCHAR(50),
 	no_hp_penumpang VARCHAR (50),
+	waktu_dibuat DATETIME,
+	waktu_diedit DATETIME,
     FOREIGN KEY (id_penerbangan) REFERENCES Penerbangan(id_penerbangan),
     FOREIGN KEY (id_penumpang) REFERENCES Penumpang(id_penumpang)
 );
@@ -48,20 +53,23 @@ CREATE TABLE Tiket (
 CREATE TABLE Pembayaran (
     id_pembayaran INT IDENTITY(1,1) PRIMARY KEY,
     id_tiket INT,
+	nama_penumpang VARCHAR(50),
+	harga_tiket DECIMAL(10,2),
     tanggal_pembayaran DATETIME,
-    harga DECIMAL(10,2),
     statuss VARCHAR(10) CHECK (statuss IN ('Sukses', 'Gagal')),
-    metode_pembayaran VARCHAR(10) CHECK (metode _pembayaran IN ('Transfer', 'Cash')),
-    FOREIGN KEY (id_tiket) REFERENCES Tiket(id_tiket)
+    metode_pembayaran  VARCHAR(10) CHECK (metode_pembayaran IN ('Transfer', 'Cash')),
+    FOREIGN KEY (id_tiket) REFERENCES Tiket(id_tiket),
 );
 
 
 CREATE TABLE Log_Penerbangan (
     id_log INT IDENTITY(1,1) PRIMARY KEY,
-    id_penerbangan INTEGER,
+    id_penerbangan INT,
     no_penerbangan VARCHAR(50),
     tempat_tujuan VARCHAR(50),
     bandara_tujuan VARCHAR(50),
 	statuss VARCHAR(25) CHECK (statuss IN ('Take off','In Flight', 'Landing')),
-    FOREIGN KEY (id_penerbangan) REFERENCES Penerbangan(id_penerbangan)
+	waktu_dibuat DATETIME DEFAULT GETDATE(),
+	waktu_diedit DATETIME DEFAULT GETDATE(),
+   FOREIGN KEY (id_penerbangan) REFERENCES Penerbangan(id_penerbangan)
 );
